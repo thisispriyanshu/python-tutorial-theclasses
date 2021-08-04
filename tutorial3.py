ea1e46fdce64931f8bfced5d343f0cb7a5d09efb
Priyanshu Agrawal <priyanshuagrawal801@gmail.com>
2021-08-04 23:42:07 +0530

    Add files via upload

---
+++ tutorial3.py
@@ -0,0 +1,28 @@
+#first python program
+#simple calculator
+#comments single line comment
+#escape sequences
+#print
+#taking input from user
+#variables
+#datatypes
+#typecasting
+#format
+
+
+# print("WELCOME TO THE CLASSES \nHELLO \tEVERYONE")
+# print("Enter the value of a and b")
+a=int(input("Enter the value of a"))
+b=int(input("Enter the value of b"))
+
+print("SUM= {}".format(a+b))
+print("DIFFERENCE= {}".format(a-b))
+print("PRODUCT= {}".format(a*b))
+print("QUOTIENT= {}".format(a/b))
+
+"""
+int a =8;  C AND C++
+MULTI LINE COMMENT 
+WELCOME TO THE CLASSES
+HELLO WORLD
+"""
